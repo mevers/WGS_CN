b4050df3609c4cf7f13ad5756bbc6141d17ff648
Maurits Evers <maurits.evers@anu.edu.au>
2017-06-16 14:28:10 +1000

    Added snakemake rules and config files

---
+++ snakemake/run_snakemake.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+snakemake --dag | dot -Tpdf > dag.pdf
+snakemake --rulegraph | dot -Tpdf > rulegraph.pdf
+
+snakemake --configfile config.yaml \
+	  --snakefile Snakefile \
+	  --jobs 10 \
+	  --printshellcmds \
+	  --cluster "qsub -pe threads {cluster.threads} \
+                          -q {cluster.queue} \
+                          -l virtual_free={cluster.virtual_free} \
+                          -l h_vmem={cluster.h_vmem} \
+                          -o {cluster.outstream} \
+                          -e {cluster.errorstream}" \
+          --cluster-config cluster.yaml
+
